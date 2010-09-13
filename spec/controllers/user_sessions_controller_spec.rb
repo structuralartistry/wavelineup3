356d260c303a0f18fbf801992695dc094cd317a7
StructuralArtistry <dk@structuralartistry.com>
2010-09-13 12:27:43 -0500

    added test for login

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -3,14 +3,25 @@ require 'spec_helper'
 describe UserSessionsController do
   render_views
 
-  describe "GET new (login)" do
-    it "shows the login form" do
+  describe 'GET new (login)' do
+    it 'shows the login form' do
       get :new
       response.should have_selector('input', :id => 'user_session_login')
       response.should have_selector('input', :id => 'user_session_password', :type => 'password')
       response.should have_selector('input', :type => 'submit', :value => 'Login')
     end
   end
+  
+  describe 'POST create (login)' do
+    it 'successfully logs the user in' do
+      user = Factory.create(:user)
+      post :create, { :user_session => { :login => user.login, :password => user.password } }
+      response.should redirect_to(root_url)
+      flash[:notice].should == 'Successfully logged in'
+    end
+  end
+  
+  
 
 end
 
