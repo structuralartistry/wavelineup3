2b7c35cc3993854bfecb0d2ae4d7dda62602fe00
StructuralArtistry <dk@structuralartistry.com>
2010-09-13 11:41:16 -0500

    added user session spec

---
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe UserSessionsController do
+  render_views
+
+  describe "GET new (login)" do
+    it "shows the login form" do
+      get :new
+      response.should have_selector('input', :id => 'user_session_login')
+      response.should have_selector('input', :id => 'user_session_password', :type => 'password')
+      response.should have_selector('input', :type => 'submit', :value => 'Login')
+    end
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+# 
+# def test_login_form_loads
+#   get :new
+#   assert :success
+#   assert_tag :tag => 'input', :attributes => { :id => 'user_session_login'}
+#   assert_tag :tag => 'input', :attributes => { :id => 'user_session_password', :type => 'password' }
+#   assert_tag :tag => 'input', :attributes => { :value => 'Login' }
+# end
+# 
+# def test_login_and_logout_successful
+#   user = Factory.create(:user)
+#   post :create, { :user_session => { :login => user.login, :password => user.password } }
+#   assert :success
+#   assert_equal flash[:notice], 'Successfully logged in'
+#   
+#   
+#   
+#   delete :destroy, :id => 0
+#   assert_equal flash[:notice], 'Successfully logged out'
+# end
+# 
+# def test_login_failed
+#   
+# end
