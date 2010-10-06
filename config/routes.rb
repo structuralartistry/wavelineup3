c306e84a93fe31d7eaf97da90bd456207d122b96
StructuralArtistry <dk@structuralartistry.com>
2010-10-06 11:41:52 -0500

    modify routes to redirect http to https in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Wavelineup3::Application.routes.draw do
   
+  if RAILS_ENV == 'production'
+    match "*", :to => redirect { |_, request| "https://" + request.host_with_port + request.fullpath }
+  end
+
   RAILS_ENV == 'production' ? protocol = 'https' : protocol = 'http'
   scope :constraints => { :protocol => protocol } do 
 
