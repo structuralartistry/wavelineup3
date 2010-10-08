6317a391d831f2a2a24396c9838f27981a6bc4e6
StructuralArtistry <dk@structuralartistry.com>
2010-10-08 14:42:41 -0500

    changes to css/app.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
   $('a').button();
   $('a').removeClass('ui-corner-all') // take off the rounded corners
-  
+
+
+  // for touchscreens - highlight on mouse down
   $('.selector_cell, .selector_cell_add, .selector_cell_delete, .selector_cell_save, .selector_cell_cancel, .selector_cell_nav').mousedown(function() {
-    $(this).addClass("item_hover");
+    $(this).addClass("item_highlight");
+  });
+  // for web - highlight on mouse over
+  $('.selector_cell, .selector_cell_add, .selector_cell_delete, .selector_cell_save, .selector_cell_cancel, .selector_cell_nav').mouseover(function() {
+    $(this).addClass("item_highlight");
     $(this).css("cursor", "pointer");
   });
   $('.selector_cell, .selector_cell_add, .selector_cell_delete, .selector_cell_save, .selector_cell_cancel, .selector_cell_nav').mouseout(function() {
-    $(this).removeClass("item_hover");
-  });  
+    $(this).removeClass("item_highlight");
+  });
+  
+
+
 });
