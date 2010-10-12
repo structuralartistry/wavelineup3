01a76bc7b6210c5d17a6a5d0cfe35f7f73b44180
StructuralArtistry <dk@structuralartistry.com>
2010-10-12 11:33:59 -0500

    incremental work on travel card, implement autosave, tests passing

--- public/javascripts/travel_card.js
+++ public/javascripts/travel_card.js
@@ -8,6 +8,7 @@ function show_selector(selector_name, calling_object, element_to_update_inner_ht
       break;
     case "side_selector":
       left_right_toggle_selected_value(calling_object);
+      
       break;
     default:
       if(selector_name=="phase_gateway_selector") {
@@ -61,7 +62,9 @@ function left_right_toggle_selected_value(object) {
 function set_selected_value(object) {
   value = $(object).html();
   $('#' + element_to_update_inner_html).html(value); 
-  $("#" + control_to_update).val(value);
+  $('#' + control_to_update).val(value);
+
+  autosave(control_to_update, $('#' + control_to_update).val());
   
   element_to_update_inner_html='';
   control_to_update=''; 
@@ -120,3 +123,39 @@ function set_third_bme_strategy() {
     $('#third_bme_strategy').html(remaining_strategy);
   }  
 }
+
+
+function autosave(control_id, value) {
+  // ascertain the controller, field name and record id to be saved
+  controller = "";
+  record_id = "";
+  field = "";
+      
+  if(control_id.indexOf("travel_card")>=0) {
+    controller = "travel_cards";
+    qualified_field = "travel_card[" + control_id.replace("travel_card_", "") + "]";
+    record_id = $("#travel_card_identity_id").val();
+    update_field_to_server(controller, record_id, qualified_field, value);
+    return;
+  }
+  
+}
+
+// AUTOSAVE FUNCTION
+// every time a field is changed, post the form... later may want to make this more efficient
+// this uese rails convention with the assumption of an update method for all controllers which
+function update_field_to_server(controller, record_id, qualified_field, value) {
+  // format the data to put
+  field_data = qualified_field + "=" + value;
+    
+  $.ajax({
+            type: "PUT",
+            url: "/" + controller + "/" + record_id,
+            data: field_data,
+            dataType: "script"
+            //success: alert(msg),
+            //failure: alert(msg)
+  });
+  
+}
+
