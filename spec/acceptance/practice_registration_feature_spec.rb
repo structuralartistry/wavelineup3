837f6d822c0c31352ebe9f43ff535f43f0621758
DK <dk@structuralartistry.com>
2010-12-17 17:04:13 -0600

    complete practice registration steak spec

--- spec/acceptance/practice_registration_feature_spec.rb
+++ spec/acceptance/practice_registration_feature_spec.rb
@@ -29,39 +29,21 @@ feature "Practice Registration Feature", %q{
     assert has_text?('Practice was successfully created. Please check your email for the activation link.')
   end
   
+  scenario "Can not register an existing practice name" do
+    Factory.create(:practice_admin_user)
+    Factory.create(:practice_one)
+    
+    visit('/login')
+    click_selector_cell('Create My Practice')
+    
+    fill_in('Practice name', :with => 'Practice One')
+    fill_in('Email', :with => 'practice@structuralartistry.com')
+    fill_in('Password', :with => 'password1')
+    fill_in('Password confirmation', :with => 'password1')
+    click_button('Submit')
+    
+    assert has_text?('Practice name has already been taken')
+    
+  end
+  
 end
-
-
-
-#     When I fill in "practice_name" with "Practice One"
-#     And I fill in "practice_users_attributes_0_email" with "practice@structuralartistry.com"
-#     And I fill in "practice_users_attributes_0_password" with "password1"
-#     And I fill in "practice_users_attributes_0_password_confirmation" with "password1"
-#     When I press "Submit"
-#     Then I should see "Practice was successfully created. Please check your email for the activation link."
-#     
-#     # activate primary user for the new practice
-#     Given I check my email "practice@structuralartistry.com" and activate my user
-#     
-#     # verify success
-#     Then I should see "Practice One" within a selector cell
-#     And I should see "practice@structuralartistry.com" within a selector cell
-#     When I click "Practice One" within a selector cell
-#     Then I should see "Manage Practice"
-#     And I should see "practice@structuralartistry.com" within "td"
-#     And I should not see "Email" within "label"
-#     And I should not see "Password" within "label"
-#     And I should not see "Password confirmation" within "label"
-#     
-#     
-#   Scenario: A practitioner tries to register a practice with a name which already exists
-#     Given there is a practice in the system named "Practice One"
-#     Given I am not logged in
-#     When I go to the new practice page
-#     When I fill in "practice_name" with "Practice One"
-#     And I fill in "practice_users_attributes_0_email" with "practice@structuralartistry.com"
-#     And I fill in "practice_users_attributes_0_password" with "password1"
-#     And I fill in "practice_users_attributes_0_password_confirmation" with "password1"
-#     When I press "Submit"
-#     Then I should see "Practice name has already been taken"
-#     
