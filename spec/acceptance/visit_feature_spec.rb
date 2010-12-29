712eeee3f834f3ecc74a1b1e677c46566976dba7
DK <dk@structuralartistry.com>
2010-12-29 10:28:07 -0600

    add steak specs to visit

--- spec/acceptance/visit_feature_spec.rb
+++ spec/acceptance/visit_feature_spec.rb
@@ -15,13 +15,9 @@ feature "Visit Feature", %q{
       confirm_visit_loaded   
     end
     
-    scenario "I can set Phase 1 values and they autosave" do
-      
+    scenario "I can set Phase 1 values and they autosave" do     
       get_selector_cell_text('selected_phase_1').should == ''
       click_selector_cell('selected_phase_1')
-
-# need to change all verifications to id's
-# broken test... just check by id, dont worry if it is a selector cell....
 
       selector_cell_present?('select_phase_1_2_C1').should == true
       selector_cell_present?('select_phase_1_2_C5').should == true
@@ -37,8 +33,6 @@ feature "Visit Feature", %q{
       click_selector_cell('select_phase_3')
       get_selector_cell_text('selected_phase_1').should == '3'
 
-
-
       get_selector_cell_text('selected_phase_1_gateway_1').should == ''
       click_selector_cell('selected_phase_1_gateway_1')
 
@@ -51,9 +45,6 @@ feature "Visit Feature", %q{
 
       click_selector_cell('select_gateway_s1')
       get_selector_cell_text('selected_phase_1_gateway_1').should == 'S1'
-
-
-
 
       get_selector_cell_text('selected_phase_1_gateway_2').should == ''
       click_selector_cell('selected_phase_1_gateway_2')
@@ -68,17 +59,8 @@ feature "Visit Feature", %q{
 
       click_selector_cell('select_gateway_apex')
       get_selector_cell_text('selected_phase_1_gateway_2').should == 'APEX'
-
-
-      # phase 3 should autopopulate as there is only one choice
-      get_selector_cell_text('selected_phase_1_direction').should == 'L/B'
       
-      # phase 1 should affected gateway cells
-      
-      
-      # phase 5 has not direction
-    
-
+      get_selector_cell_text('selected_phase_1_direction').should == 'L/B' # auto set
 
       get_selector_cell_text('selected_phase_1_level_of_care').should == ''
       click_selector_cell('selected_phase_1_level_of_care')
@@ -96,14 +78,93 @@ feature "Visit Feature", %q{
       
       click_selector_cell('select_level_of_care_3a')
       get_selector_cell_text('selected_phase_1_level_of_care').should == '3A'
+    end
+    
+    scenario "selected phase 3 should autopopulate direction" do
+      # phase 1
+      click_selector_cell('selected_phase_1')
+      click_selector_cell('select_phase_3')
+      get_selector_cell_text('selected_phase_1').should == '3'
+      get_selector_cell_text('selected_phase_1_direction').should == 'L/B'
+      
+      # phase 2
+      click_selector_cell('work_second_phase_button')
+      click_selector_cell('selected_phase_2')
+      click_selector_cell('select_phase_3')
+      get_selector_cell_text('selected_phase_2').should == '3'
+      get_selector_cell_text('selected_phase_2_direction').should == 'L/B'
+    end
+    
+    scenario "selected phase 1 > x should make available affected gateway selectors" do
+      # phase 1
+      has_text?('Aff', 'td').should == false
+      selector_cell_present?('selected_phase_1_gateway_1_affecting').should == false
+      click_selector_cell('selected_phase_1')
+      click_selector_cell('select_phase_1_2_C1')
+      get_selector_cell_text('selected_phase_1').should == '1 > 2 C1'
+      
+      has_text?('Aff', 'td').should == true
+      
+      # gw aff 1
+      selector_cell_present?('selected_phase_1_gateway_1_affecting').should == true
+      get_selector_cell_text('selected_phase_1_gateway_1_affecting').should == ''
+      click_selector_cell('selected_phase_1_gateway_1_affecting')
+      selector_cell_present?('select_gateway_c1_occ').should == true
+      selector_cell_present?('select_gateway_c1_c2').should == true
+      click_selector_cell('select_gateway_c1_c2')
+      get_selector_cell_text('selected_phase_1_gateway_1_affecting').should == 'C1/C2'
+      # gw aff 2
+      selector_cell_present?('selected_phase_1_gateway_2_affecting').should == true
+      get_selector_cell_text('selected_phase_1_gateway_2_affecting').should == ''
+      click_selector_cell('selected_phase_1_gateway_2_affecting')
+      selector_cell_present?('select_gateway_c1_occ').should == true
+      selector_cell_present?('select_gateway_c1_c2').should == false # limited out of list based on first selection
+      click_selector_cell('select_gateway_c1_occ')
+      get_selector_cell_text('selected_phase_1_gateway_2_affecting').should == 'C1/OCC'
       
       
+      # phase 2
+      click_selector_cell('work_second_phase_button')
+      selector_cell_present?('selected_phase_2_gateway_1_affecting').should == false
+      click_selector_cell('selected_phase_2')
+      click_selector_cell('select_phase_1_2_C1')
+      get_selector_cell_text('selected_phase_2').should == '1 > 2 C1'
+    
+      # gw aff 1
+      selector_cell_present?('selected_phase_2_gateway_1_affecting').should == true
+      get_selector_cell_text('selected_phase_2_gateway_1_affecting').should == ''
+      click_selector_cell('selected_phase_2_gateway_1_affecting')
+      selector_cell_present?('select_gateway_c1_occ').should == true
+      selector_cell_present?('select_gateway_c1_c2').should == true
+      click_selector_cell('select_gateway_c1_c2')
+      get_selector_cell_text('selected_phase_2_gateway_1_affecting').should == 'C1/C2'
+      # gw aff 2
+      selector_cell_present?('selected_phase_2_gateway_2_affecting').should == true
+      get_selector_cell_text('selected_phase_2_gateway_2_affecting').should == ''
+      click_selector_cell('selected_phase_2_gateway_2_affecting')
+      selector_cell_present?('select_gateway_c1_occ').should == true
+      selector_cell_present?('select_gateway_c1_c2').should == false # limited out of list based on first selection
+      click_selector_cell('select_gateway_c1_occ')
+      get_selector_cell_text('selected_phase_2_gateway_2_affecting').should == 'C1/OCC'
+    end
+    
+    scenario "selected phase 5 has no direction" do
+      # phase 1
+      has_text?('Dir', 'td').should == true
+      click_selector_cell('selected_phase_1')
+      click_selector_cell('select_phase_5')
+      get_selector_cell_text('selected_phase_1').should == '5'
       
+      has_text?('Dir', 'td').should == false
       
-      # additional logic
+      # phase 2
+      click_selector_cell('work_second_phase_button')
+      has_text?('Dir', 'td').should == true
+      click_selector_cell('selected_phase_2')
+      click_selector_cell('select_phase_5')
+      get_selector_cell_text('selected_phase_2').should == '5'
       
-
-      
+      has_text?('Dir', 'td').should == false
     end
     
   
