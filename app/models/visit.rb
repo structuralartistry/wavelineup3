11ee7ecda3609f49098f5cbb57702cb5b8e78eff
DK <dk@structuralartistry.com>
2010-12-31 11:28:52 -0600

    add validation for visit date

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,5 +1,6 @@
 class Visit < ActiveRecord::Base
   belongs_to :practice_member
   
-  
+  validates_presence_of :date
+  validates_format_of :date, :with => /[0-9]{4}\-[0-9]{2}\-[0-9]{2}/, :message => 'must be in format YYYY-MM-DD'
 end
--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -2,7 +2,17 @@ require 'spec_helper'
 
 describe Visit do
   
+  before(:each) do
+    Factory.create(:visit)
+  end
+  
   it { should belong_to(:practice_member) }
+  
+  it { should validate_presence_of(:date) }
+  it { should allow_value('2010-12-31').for(:date) }
+  it { should_not allow_value('12/31/2010').for(:date) }
+  it { should_not allow_value('abcde').for(:date) }
+  
   
   describe "load todays visit" do
     
