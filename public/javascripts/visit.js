dc492fbb4ec90cbd85fa82816118e4944e83dae2
David Kahn <dk@structuralartistry.com>
2011-05-22 18:32:06 -0500

    fix bug not clearing dominant occiput highlight on mini travel card

--- public/javascripts/visit.js
+++ public/javascripts/visit.js
@@ -29,10 +29,10 @@ function mini_travel_card_set_values_from_hidden_fields() {
 function set_mini_travel_card_text_and_field_color_class(field_id, side_value) {
 	// remove all color classes
 	$('#' + field_id).removeClass('mini_travel_card_cell').removeClass('L').removeClass('R');
-	
+
 	// set the value
 	$('#' + field_id).html(side_value);
-	
+
 	// set the css color
 	if(side_value=='L' || side_value=='R') $('#' + field_id).addClass(side_value);
 	else 	$('#' + field_id).addClass('mini_travel_card_cell');
@@ -103,7 +103,7 @@ var visit_gateway_currently_working;
 function set_phase_and_gateway_currently_working() {
   if(element_to_update_inner_html.indexOf("phase_1")>=0) visit_phase_currently_working = "1";
   else if(element_to_update_inner_html.indexOf("phase_2")>=0) visit_phase_currently_working = "2";
-  
+
   if(element_to_update_inner_html.indexOf("gateway_1_affecting")>=0) visit_gateway_currently_working = "1_affecting";
   else if(element_to_update_inner_html.indexOf("gateway_2_affecting")>=0) visit_gateway_currently_working = "2_affecting";
   else if(element_to_update_inner_html.indexOf("gateway_1")>=0) visit_gateway_currently_working = "1";
@@ -113,21 +113,21 @@ function set_phase_and_gateway_currently_working() {
 // HIGHLIGHT GATEWAYS
 
 function highlight_available_gateways() {
-  
+
   clear_available_gateways();
 
   // get the phase selected on the visit
   current_phase = $('#visit_phase_' + visit_phase_currently_working).val();
-  
+
   switch(current_phase)
   {
     // for now no phase 1 - as just about all gateways are available
-      
+
     case "2 C1":
       $("#mini_travel_card_gateway_c1_occ").addClass("highlighted");
       $("#mini_travel_card_gateway_c1_c2").addClass("highlighted");
       break;
-    
+
     case "2 C5":
       $("#mini_travel_card_gateway_c4_c5").addClass("highlighted");
       $("#mini_travel_card_gateway_c5_c4").addClass("highlighted");
@@ -138,7 +138,7 @@ function highlight_available_gateways() {
       $("#mini_travel_card_gateway_c7_t1").addClass("highlighted");
       $("#mini_travel_card_gateway_t1_c7").addClass("highlighted");
       break;
-      
+
     case "3":
       $("#mini_travel_card_gateway_s1").addClass("highlighted");
       $("#mini_travel_card_gateway_s2").addClass("highlighted");
@@ -147,20 +147,20 @@ function highlight_available_gateways() {
       $("#mini_travel_card_gateway_s5").addClass("highlighted");
       $("#mini_travel_card_gateway_cx").addClass("highlighted");
       break;
-    
+
     case "4":
-      $("#mini_travel_card_gateway_c2_c1").addClass("highlighted"); 
+      $("#mini_travel_card_gateway_c2_c1").addClass("highlighted");
       $("#mini_travel_card_gateway_c2_c3").addClass("highlighted");
       $("#mini_travel_card_gateway_c3_c2").addClass("highlighted");
       $("#mini_travel_card_gateway_c3_c4").addClass("highlighted");
       break;
-      
+
     case "5":
 			$("#mini_travel_card_dominant_occiput").addClass("highlighted");
       $("#mini_travel_card_gateway_occ_c1").addClass("highlighted");
       $("#mini_travel_card_gateway_c1_occ").addClass("highlighted");
       $("#mini_travel_card_gateway_c1_c2").addClass("highlighted");
-      $("#mini_travel_card_gateway_c2_c1").addClass("highlighted"); 
+      $("#mini_travel_card_gateway_c2_c1").addClass("highlighted");
       $("#mini_travel_card_gateway_c2_c3").addClass("highlighted");
       $("#mini_travel_card_gateway_c3_c2").addClass("highlighted");
       $("#mini_travel_card_gateway_c3_c4").addClass("highlighted");
@@ -178,10 +178,11 @@ function highlight_available_gateways() {
 }
 
 function clear_available_gateways() {
-  $("#mini_travel_card_gateway_occ_c1").removeClass("highlighted"); 
+  $("#mini_travel_card_dominant_occiput").removeClass("highlighted");
+  $("#mini_travel_card_gateway_occ_c1").removeClass("highlighted");
   $("#mini_travel_card_gateway_c1_occ").removeClass("highlighted");
   $("#mini_travel_card_gateway_c1_c2").removeClass("highlighted");
-  $("#mini_travel_card_gateway_c2_c1").removeClass("highlighted"); 
+  $("#mini_travel_card_gateway_c2_c1").removeClass("highlighted");
   $("#mini_travel_card_gateway_c2_c3").removeClass("highlighted");
   $("#mini_travel_card_gateway_c3_c2").removeClass("highlighted");
   $("#mini_travel_card_gateway_c3_c4").removeClass("highlighted");
@@ -206,37 +207,37 @@ function clear_available_gateways() {
 
 // PHASE
 // not using universal functions here to show/set/clear as there is special logic needed
-function set_phase(selected_phase) {	
+function set_phase(selected_phase) {
 	set_selected_value_manual('selected_phase_' + visit_phase_currently_working, 'visit_phase_' + visit_phase_currently_working, selected_phase);
-  
+
   set_visible_fields_per_selected_phase(selected_phase, visit_phase_currently_working);
-    
+
   $('#phase_' + visit_phase_currently_working + '_selector').hide();
-  
-  // clear selectors which are dependant on phase 
+
+  // clear selectors which are dependant on phase
   clear_phase_gateway(visit_phase_currently_working, "1");
   clear_phase_gateway(visit_phase_currently_working, "1_affecting");
   clear_phase_gateway(visit_phase_currently_working, "2");
   clear_phase_gateway(visit_phase_currently_working, "2_affecting");
-  
+
   clear_phase_direction(visit_phase_currently_working);
   $("#visit_stats_phase_" + visit_phase_currently_working).html("");
-  
+
   // set available gateways
-  highlight_available_gateways();	
+  highlight_available_gateways();
 
 	// set the direction choices -- this is mainly for phase 3 where there is one choice and we want to autopopulate it
 	set_phase_direction_selector_choices(visit_phase_currently_working, selected_phase);
 }
 
 function set_visible_fields_per_selected_phase(phase, visit_phase) {
-  
+
   if(phase==undefined) phase = "";
-  
+
   // allow for the visit_phase parameter - mainly used for the page load initial state setting
   // where 2nd phase should not assume phase 1 (and therefore not show gateway affecting fields)
   if(visit_phase==undefined) visit_phase = visit_phase_currently_working;
-  
+
   if(phase.indexOf('1 >')>=0) {
     // show 'gateway affecting' labels and fields for Phase 1
     $(".phase_" + visit_phase + "_affected_gateway_label").show();
@@ -248,7 +249,7 @@ function set_visible_fields_per_selected_phase(phase, visit_phase) {
     $(".phase_" + visit_phase + "_affected_gateway_label").hide();
     $("#selected_phase_" + visit_phase + "_gateway_1_affecting").hide();
     $("#selected_phase_" + visit_phase + "_gateway_2_affecting").hide();
-    
+
     if(phase=="5") {
       // do not show direction - it is implied by the gateways used
       $("#phase_" + visit_phase + "_direction_label").hide();
@@ -259,7 +260,7 @@ function set_visible_fields_per_selected_phase(phase, visit_phase) {
       $("#phase_" + visit_phase + "_direction_label").show();
       $("#selected_phase_" + visit_phase + "_direction").show();
     }
-    
+
   }
 }
 
@@ -294,11 +295,11 @@ function set_phase_gateway(calling_object) {
   gateway_value = $(calling_object).html();
 	value_to_save = remove_span_from_gateway_html(gateway_value);
   $('#visit_phase_' + visit_phase_currently_working + '_gateway_' + visit_gateway_currently_working).val(value_to_save);
-  $('#selected_phase_' + visit_phase_currently_working + '_gateway_' + visit_gateway_currently_working).html(gateway_value); 
+  $('#selected_phase_' + visit_phase_currently_working + '_gateway_' + visit_gateway_currently_working).html(gateway_value);
 	autosave('visit_phase_' + visit_phase_currently_working + '_gateway_' + visit_gateway_currently_working, value_to_save);
 
   $('#phase_' + visit_phase_currently_working + '_gateway_' + visit_gateway_currently_working + '_selector').hide();
-    
+
   // if gateway 2 is set to same as what has been selected for this gateway, clear gateway 2
   // we only want to do this if not working gateway 2
   if(visit_gateway_currently_working=="1") {
@@ -308,8 +309,8 @@ function set_phase_gateway(calling_object) {
       clear_phase_gateway(visit_phase_currently_working,"2");
     }
   }
-  
-  // do the same as above for gateway 2 affecting 
+
+  // do the same as above for gateway 2 affecting
   if(visit_gateway_currently_working=="1_affecting") {
     selected_phase_gateway_2_affecting = $('#visit_phase_' + visit_phase_currently_working + '_gateway_2_affecting').val();
     if(selected_phase_gateway_2_affecting==gateway_value) {
@@ -317,30 +318,30 @@ function set_phase_gateway(calling_object) {
       clear_phase_gateway(visit_phase_currently_working,"2_affecting");
     }
   }
-  
-  
-  $(".selector").hide(); 
+
+
+  $(".selector").hide();
 }
 
 function clear_phase_gateway(visit_phase, visit_gateway) {
   // remove all gateway settings for selected phase and gateway
   // inputs are '1' or '2' for phase (this is not the NSA phase but the phase in the visit)
   // and '1' or '2' for gateway
-  
+
   // if signature params are defined, these override the global 'visit_phase...' and 'visit_gateway...'
   if(visit_phase==undefined) visit_phase = visit_phase_currently_working;
   if(visit_gateway==undefined) visit_gateway = visit_gateway_currently_working;
-  
+
   $('#visit_phase_' + visit_phase + '_gateway_' + visit_gateway).val('');
-  $('#selected_phase_' + visit_phase + '_gateway_' + visit_gateway).html(''); 
+  $('#selected_phase_' + visit_phase + '_gateway_' + visit_gateway).html('');
   set_phase_gateway_selector_choices('1', '', '');
-  $(".selector").hide();  
+  $(".selector").hide();
 }
 
 function set_phase_gateway_selector_choices(selected_phase) {
 
   // set the 'filter for' - this is the name of the field which should not be duplicated,
-  // i.e. the selector choises for gateway 2 should not have in its array the selected 
+  // i.e. the selector choises for gateway 2 should not have in its array the selected
   // value of gateway 1, same for gateway 2 affected against gateway 1 affected
   limit_gateway = "";
   // get the gateway or gateway affecting (if 2, then is 1, if 2 affecting, is 1 affecting to limit)
@@ -349,22 +350,22 @@ function set_phase_gateway_selector_choices(selected_phase) {
   if(limit_gateway!="") {
 		// do not filter in these phases: 1 > 2 C1, 1 > 2 C5, 1 > 4, as the second gateway affected could be same as the first
 		if(selected_phase!='1 > 2 C1' && selected_phase!='1 > 2 C5' && selected_phase!='1 > 4') {
-    	filter_for_gateway = $('#visit_phase_' + visit_phase_currently_working + '_gateway_' + limit_gateway).val();    
+    	filter_for_gateway = $('#visit_phase_' + visit_phase_currently_working + '_gateway_' + limit_gateway).val();
   	}
 	}
   else filter_for_gateway = "";
 
   $(".gateway_selection_cell").hide();
-  
+
   // always show the blank selector element. if no phase, prompt for phase
-  $('#select_gateway_').show();  
+  $('#select_gateway_').show();
   if(selected_phase=='') {
     $('#select_gateway_').html('please select a phase');
   }
   else {
     $('#select_gateway_').html(' ');
   }
-  
+
   switch(selected_phase)
   {
     case "1 > 2 C1":
@@ -386,7 +387,7 @@ function set_phase_gateway_selector_choices(selected_phase) {
           break;
       }
       break;
-      
+
     case "1 > 2 C5":
       switch(visit_gateway_currently_working) {
         case "1":
@@ -414,11 +415,11 @@ function set_phase_gateway_selector_choices(selected_phase) {
           if(filter_for_gateway.indexOf('T1/T2')==-1) $('#select_gateway_t1_t2').show();
           if(filter_for_gateway.indexOf('T2/T1')==-1) $('#select_gateway_t2_t1').show();
           if(filter_for_gateway.indexOf('T2/T3')==-1) $('#select_gateway_t2_t3').show();
-          if(filter_for_gateway.indexOf('T3/T2')==-1) $('#select_gateway_t3_t2').show();  
+          if(filter_for_gateway.indexOf('T3/T2')==-1) $('#select_gateway_t3_t2').show();
           break;
       }
       break;
-      
+
     case "1 > 3":
       switch(visit_gateway_currently_working) {
         case "1":
@@ -446,7 +447,7 @@ function set_phase_gateway_selector_choices(selected_phase) {
           break;
       }
       break;
-      
+
     case "1 > 4":
       switch(visit_gateway_currently_working) {
         case "1":
@@ -468,7 +469,7 @@ function set_phase_gateway_selector_choices(selected_phase) {
           break;
       }
       break;
-      
+
     case "1 > 5":
       switch(visit_gateway_currently_working) {
         case "1":
@@ -514,7 +515,7 @@ function set_phase_gateway_selector_choices(selected_phase) {
           break;
       }
       break;
-      
+
     case "2 C1":
       if(filter_for_gateway.indexOf('C1/OCC')==-1) $('#select_gateway_c1_occ').show();
       if(filter_for_gateway.indexOf('C1/C2')==-1) $('#select_gateway_c1_c2').show();
@@ -535,10 +536,10 @@ function set_phase_gateway_selector_choices(selected_phase) {
       if(filter_for_gateway.indexOf('T2/T1')==-1) $('#select_gateway_t2_t1').show();
       if(filter_for_gateway.indexOf('T2/T3')==-1) $('#select_gateway_t2_t3').show();
       if(filter_for_gateway.indexOf('T3/T2')==-1) $('#select_gateway_t3_t2').show();
-      
+
       break;
 
-    case "3": 
+    case "3":
       if(filter_for_gateway.indexOf('S1')==-1) $('#select_gateway_s1').show();
       if(filter_for_gateway.indexOf('S2')==-1) $('#select_gateway_s2').show();
       if(filter_for_gateway.indexOf('S3')==-1) $('#select_gateway_s3').show();
@@ -584,7 +585,7 @@ function set_phase_gateway_selector_choices(selected_phase) {
         // gateway 2 selections
         if(filter_for_gateway.indexOf('CX')==-1) $('#select_gateway_cx').show();
         if(filter_for_gateway.indexOf('APEX')==-1) $('#select_gateway_apex').show();
-      }     
+      }
       break;
   }
 }
@@ -607,25 +608,25 @@ function set_phase_direction(direction) {
 
 function clear_phase_direction(visit_phase) {
   $('#visit_phase_' + visit_phase + '_direction').val('');
-  
-  $('#selected_phase_' + visit_phase + '_direction').html(''); 
+
+  $('#selected_phase_' + visit_phase + '_direction').html('');
   set_phase_direction_selector_choices(visit_phase, '');
   $(".selector").hide();
 }
 
 function set_phase_direction_selector_choices(visit_phase, phase) {
-  
+
   hide_all_phase_directions(visit_phase);
-  
+
   // always show the 'remove' selector element. if no phase, prompt for phase
-  $('#select_direction_').show();  
+  $('#select_direction_').show();
   // if($('#visit_phase_' + visit_phase).val()=='') {
   //   $('#no_phase_' + visit_phase + '_direction').html('please select a phase');
   // }
   // else {
   //   $('#no_phase_' + visit_phase + '_direction').html(' ');
   // }
-  
+
   switch(phase)
   {
     case "1 > 2 C1":
@@ -636,19 +637,19 @@ function set_phase_direction_selector_choices(visit_phase, phase) {
       $('#select_direction_m').show();
       $('#select_direction_l').show();
       break;
-      
-    case "2 C1": 
+
+    case "2 C1":
     case "2 C5":
     case "4":
       $('#select_direction_f_e').show();
       $('#select_direction_l_b').show();
       break;
-      
-    case "3":   
+
+    case "3":
 			// since only one direction set it in the field -- no selector
 			$('#selected_phase_' + visit_phase + '_direction').html('L/B');
       break;
-      
+
     case "5":
       // show none as is either cx or apex and this is set in gateways
       break;
@@ -656,18 +657,18 @@ function set_phase_direction_selector_choices(visit_phase, phase) {
 }
 
 function hide_all_phase_directions(visit_phase) {
-  
+
   $('#select_direction_m').hide();
   $('#select_direction_l').hide();
   $('#select_direction_f_e').hide();
   $('#select_direction_l_b').hide();
   $('#select_direction_').hide();
-  
+
 }
 
 // SRI
 function set_selected_sri_stage(object) {
-	stage = $(object).html();	
+	stage = $(object).html();
 	set_sri_positions(stage);
 	set_selected_value(object);
 }
@@ -677,11 +678,11 @@ function set_sri_positions(stage) {
 	if(stage=='1' || stage=='2') {
 		$('#label_sri_position_b').show();
 		$('#sri_position_b').show();
-		if(stage=='1') {	
+		if(stage=='1') {
 			$('#label_sri_position_a').html('Peace');
 			$('#label_sri_position_b').html('Discon');
 		}
-		if(stage=='2') {	
+		if(stage=='2') {
 			$('#label_sri_position_a').html('Pos A');
 			$('#label_sri_position_b').html('Pos B');
 		}
