7cb65eed80098c584986056b4771d4e92fd65d32
David Kahn <dk@structuralartistry.com>
2011-06-20 10:02:08 -0500

    add additional seed data for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,26 +18,39 @@ user.role_id = Role.find_by_name('sysadmin').id
 user.active = true
 user.save
 
-puts ""
-puts "WARNING: Sysadmin has an unsecure password, '#{user.password}'. Change before production!"
-puts "It will be down root"
 
-# create a demo practice and user
-practice = Practice.create(:name => 'Demo Practice', :time_zone => 'Mexico City')
-user = User.new
-user.email = 'dk.kahn@gmail.com'
-user.password = 'Password1'
-user.password_confirmation = 'Password1'
-user.practice_id = practice.id
-user.role_id = Role.find_by_name('practice admin').id
-user.active = true
-user.save
+if RAILS_ENV='development'
+  # create a demo practice and user
+  practice = Practice.create(:name => 'Demo Practice', :time_zone => 'Mexico City')
+  user = User.new
+  user.email = 'dk.kahn@gmail.com'
+  user.password = 'Password1'
+  user.password_confirmation = 'Password1'
+  user.practice_id = practice.id
+  user.role_id = Role.find_by_name('practice admin').id
+  user.active = true
+  user.save
 
-user = User.new
-user.email = 'dnathankahn@yahoo.com'
-user.password = 'Password1'
-user.password_confirmation = 'Password1'
-user.practice_id = practice.id
-user.role_id = Role.find_by_name('practice user').id
-user.active = true
-user.save
+  user = User.new
+  user.email = 'dnathankahn@yahoo.com'
+  user.password = 'Password1'
+  user.password_confirmation = 'Password1'
+  user.practice_id = practice.id
+  user.role_id = Role.find_by_name('practice user').id
+  user.active = true
+  user.save
+
+  # populate data into test practice for seeded demo
+  last_names = %w(Apple	Buoboy	Charles	Darwin	Erickson	Frederick	Glass	Heller	Inverson	Jolie	Kahn	Lee	Ming	Norrey	Openheimer	Petersen	Quartz	Robinson	Sing	Tho	Uebbing	Vincenza	Wang	Xi	Yeller	Zetter)
+  first_names = %w(John	Barbara	Myra	Erik	John	Peter	Sonya	Linda	Mark	Jessica	David	Juan	Hong	Erika	Clemens	Malya	Robin	Rhonda	Ronald	Ving	Rocky	Maria	Yi	Xy	Michael	Ron)
+  initials = %w(A	B	C	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z)
+
+  initials.each do |initial|
+    5.times do
+      PracticeMember.create(:practice => practice,
+                            :last_name => "#{initial}#{last_names.sample.downcase}",
+                            :first_name => first_names.sample,
+                            :middle_name => initials.sample)
+    end
+  end
+end
