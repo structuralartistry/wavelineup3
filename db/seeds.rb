d161b1bc1b7f62031f5c195caf64b3132a9988a1
David Kahn <dk@structuralartistry.com>
2011-06-24 14:30:39 -0500

    mostly styline and writing about page

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,16 +41,16 @@ if RAILS_ENV='development'
   user.save
 
   # populate data into test practice for seeded demo
-  last_names = %w(Apple	Buoboy	Charles	Darwin	Erickson	Frederick	Glass	Heller	Inverson	Jolie	Kahn	Lee	Ming	Norrey	Openheimer	Petersen	Quartz	Robinson	Sing	Tho	Uebbing	Vincenza	Wang	Xi	Yeller	Zetter)
-  first_names = %w(John	Barbara	Myra	Erik	John	Peter	Sonya	Linda	Mark	Jessica	David	Juan	Hong	Erika	Clemens	Malya	Robin	Rhonda	Ronald	Ving	Rocky	Maria	Yi	Xy	Michael	Ron)
-  initials = %w(A	B	C	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z)
-
-  initials.each do |initial|
-    5.times do
-      PracticeMember.create(:practice => practice,
-                            :last_name => "#{initial}#{last_names.sample.downcase}",
-                            :first_name => first_names.sample,
-                            :middle_name => initials.sample)
-    end
-  end
+#  last_names = %w(Apple	Buoboy	Charles	Darwin	Erickson	Frederick	Glass	Heller	Inverson	Jolie	Kahn	Lee	Ming	Norrey	Openheimer	Petersen	Quartz	Robinson	Sing	Tho	Uebbing	Vincenza	Wang	Xi	Yeller	Zetter)
+#  first_names = %w(John	Barbara	Myra	Erik	John	Peter	Sonya	Linda	Mark	Jessica	David	Juan	Hong	Erika	Clemens	Malya	Robin	Rhonda	Ronald	Ving	Rocky	Maria	Yi	Xy	Michael	Ron)
+#  initials = %w(A	B	C	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z)
+#
+#  initials.each do |initial|
+#    5.times do
+#      PracticeMember.create(:practice => practice,
+#                            :last_name => "#{initial}#{last_names.sample.downcase}",
+#                            :first_name => first_names.sample,
+#                            :middle_name => initials.sample)
+#    end
+#  end
 end
