7bb489e0320a61625e4db6875336a37f22edbb43
David Kahn <dk@structuralartistry.com>
2011-07-10 17:20:22 -0500

    amplify feedback support email message

--- app/models/feedback_support.rb
+++ app/models/feedback_support.rb
@@ -10,7 +10,14 @@ class FeedbackSupport < ActiveRecord::Base
 
   def send_email_notification_to_sysadmin
     sysadmin_user = User.find_by_role_id(Role.find_by_name('sysadmin').id)
-    SystemMailer.generic_email(sysadmin_user.email, 'New Feedback/Support message', @feedback_support.inspect).deliver
+    message = <<-DOC
+      DateTime: #{self.created_at}
+      Practice Name: #{self.user.practice.name}
+      User Email: #{self.user.email}
+      Location in Application: #{self.location_in_application}
+      Message: #{self.message}
+    DOC
+    SystemMailer.generic_email(sysadmin_user.email, 'New Feedback/Support message', message).deliver
   end
 
 end
