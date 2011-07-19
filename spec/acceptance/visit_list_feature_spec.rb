4cb454afa3de7885e11e4dce1ee6d3d856c30941
David Kahn <dk@structuralartistry.com>
2011-07-19 12:03:38 -0500

    add fields to visit list

--- spec/acceptance/visit_list_feature_spec.rb
+++ spec/acceptance/visit_list_feature_spec.rb
@@ -3,30 +3,32 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 feature "Visit List Feature", %q{
 
 } do
-  
+
   context "visit list functionality", :visit_list => true, :js => true do
-    
+
     before(:each) do
       practice_name = 'StructuralArtistry practice'
       logged_in_as_role_for_practice(:practice_user, practice_name)
       @practice_member = create_practice_member('Kahn, David Nathan', practice_name)
-      @practice_room_visit_list_page = "/practice_room/#{@practice_member.id}/visit_list" 
+      @practice_room_visit_list_page = "/practice_room/#{@practice_member.id}/visit_list"
     end
 
     scenario "the visit list shows the date, phase 1, 2 and sri stage of all visits for the selected practice member" do
-      visit(@practice_room_visit_list_page) 
+      visit(@practice_room_visit_list_page)
 
       # no visits
       has_text?('No Visits on record for this Practice Member').should == true
       has_text?('Date', 'td').should == false
-      has_text?('Phase 1', 'td').should == false
-      has_text?('Phase 2', 'td').should == false
+      has_text?('1st Phase', 'td').should == false
+      has_text?('LOC', 'td').should == false
+      has_text?('Dir', 'td').should == false
+      has_text?('2nd Phase', 'td').should == false
       has_text?('SRI Stage', 'td').should == false
       has_text?('2010-01-01', 'td').should == false
       has_text?('3', 'td').should == false
       has_text?('5', 'td').should == false
       has_text?('6', 'td').should == false
-      
+
       visit = Factory.create(:visit)
       visit.practice_member_id = @practice_member.id
       visit.date = '2010-01-01'
@@ -34,19 +36,21 @@ feature "Visit List Feature", %q{
       visit.phase_2 = '5'
       visit.sri_stage = '6'
       visit.save
-      
-      visit(@practice_room_visit_list_page) 
-      
+
+      visit(@practice_room_visit_list_page)
+
       has_text?('Date', 'td').should == true
-      has_text?('Phase 1', 'td').should == true
-      has_text?('Phase 2', 'td').should == true
+      has_text?('1st Phase', 'td').should == true
+      has_text?('LOC', 'td').should == true
+      has_text?('Dir', 'td').should == true
+      has_text?('2nd Phase', 'td').should == true
       has_text?('SRI Stage', 'td').should == true
-      
+
       has_text?('2010-01-01 00:00', 'td').should == true
       has_text?('3', 'td').should == true
       has_text?('5', 'td').should == true
       has_text?('6', 'td').should == true
-      
+
       visit = Factory.create(:visit)
       visit.practice_member_id = visit.id
       visit.date = '2011-01-01'
@@ -54,9 +58,9 @@ feature "Visit List Feature", %q{
       visit.phase_2 = '4'
       visit.sri_stage = '9'
       visit.save
-      
+
       visit(@practice_room_visit_list_page)
-      
+
       has_text?('2010-01-01 00:00', 'td').should == true
       has_text?('3', 'td').should == true
       has_text?('5', 'td').should == true
@@ -71,11 +75,11 @@ feature "Visit List Feature", %q{
       visit.phase_2 = '5'
       visit.sri_stage = '6'
       visit.save
-      
-      visit(@practice_room_visit_list_page) 
-      
+
+      visit(@practice_room_visit_list_page)
+
       click_selector_cell("#{visit.id}")
-      
+
       verify_visit_loaded(visit.id)
     end
 
