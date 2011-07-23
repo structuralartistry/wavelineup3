b735707e2ce1f1626c173e41bce4c32c4f25d070
David Kahn <dk@structuralartistry.com>
2011-07-23 12:49:40 -0500

    remove non used files

--- spec/factories/feature_request.rb
+++ spec/factories/feature_request.rb
@@ -2,7 +2,7 @@ Factory.define :feature_request do |f|
   f.name 'feature name here'
   f.description 'feature description here'
   f.notes 'feature notes here'
-  f.times_requested 1
+  f.popularity_priority_index 1
 end
 
 Factory.define :feature_request_implemented, :parent => :feature_request do |f|
--- spec/models/login_spec.rb
+++ spec/models/login_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Login do
-  it { should have_many(:users) }
+  it { should belong_to(:user) }
 end
