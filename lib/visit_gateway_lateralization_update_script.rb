e405527239d0e41479cb5c6ef40cca77b9907996
David Kahn <dk@structuralartistry.com>
2011-07-24 14:07:15 -0500

    fix script

--- lib/visit_gateway_lateralization_update_script.rb
+++ lib/visit_gateway_lateralization_update_script.rb
@@ -23,19 +23,19 @@ def update_past_visits
                       visit.phase_2_gateway_2_affecting)
     set_gateways.each do |set_visit_gateway|
       puts "  set_visit_gateway: #{set_visit_gateway}"
-      set_gateway_text = eval(set_visit_gateway)
+      set_gateway_text = eval(set_visit_gateway).strip
       puts "    set_gateway_text: #{set_gateway_text}"
-      if set_gateway_text
+      if set_gateway_text && !set_gateway_text.empty?
         normalized_gateway = 'gateway_' + set_gateway_text.downcase.gsub(/\//,'_')
         puts "    normalized_gateway: #{normalized_gateway}"
         gateway_side = eval("travel_card.#{normalized_gateway}").strip
         puts "    gateway_side: #{gateway_side}"
         if gateway_side =~ /[L]/
-          eval_expn = "set_visit_gateway = 'L ' + #{set_gateway_text}"
+          eval_expn = "set_visit_gateway = 'L #{set_gateway_text}'"
           puts "    eval_expn: #{eval_expn}"
           eval(eval_expn)
         elsif gateway_side =~ /[R]/
-          eval_expn = "set_visit_gateway = 'R ' + #{set_gateway_text}"
+          eval_expn = "set_visit_gateway = 'R #{set_gateway_text}"
           puts "    eval_expn: #{eval_expn}"
           eval(eval_expn)
         end
