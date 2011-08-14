b0971905aa8cce226a530a5f3723013312d34203
structuralartistry <dk@structuralartistry.com>
2011-08-14 14:06:03 -0400

    order features implemented by date descending

--- app/models/feature_request.rb
+++ app/models/feature_request.rb
@@ -1,5 +1,5 @@
 class FeatureRequest < ActiveRecord::Base
 
-  scope :recently_implemented, where('date_implemented IS NOT NULL AND date_implemented >= ?', Date.today-7.days)
+  scope :recently_implemented, where('date_implemented IS NOT NULL AND date_implemented >= ?', Date.today-7.days).order('date_implemented DESC')
 
 end
